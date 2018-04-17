f32c1442a4d289512707d4e45914bce0f3a7f600
Pospolity <pospolity@outlook.com>
2018-04-17 21:02:23 +0200

    Hello SFML

--- main.cpp
+++ main.cpp
@@ -2,9 +2,34 @@
 // Created on 17/04/2018.
 //
 
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    sf::RenderWindow window(sf::VideoMode(640,480),"Hello SFML");
+
+    sf::Font font;
+    font.loadFromFile("assets/fonts/OpenSans-Bold.ttf");
+
+    sf::Text text("Hello SFML",font,11);
+    text.setCharacterSize(64);
+    text.setPosition(window.getSize().x/2 - text.getGlobalBounds().width/2,
+                     window.getSize().y/2 - text.getGlobalBounds().height/2);
+
+    while(window.isOpen()){
+
+        sf::Event event;
+        while(window.pollEvent(event)) {
+            if(event.type == sf::Event::Closed){
+                window.close();
+            }
+
+            window.clear(sf::Color::Black);
+            window.draw(text);
+            window.display();
+        }
+    }
+
     return 0;
+
 }
